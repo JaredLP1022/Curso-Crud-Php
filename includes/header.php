8d0b45a71bb83e4371b505e7a0c3f3bb8254b581
JaredLP1022 <JaredPoseros@yahoo.com>
2024-06-02 22:44:54 -0600

    Se agregaron los comentarios al header para saber algunas caracteristicas de la aplicacion web

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
+<!-- Aqui se establece el idioma de nuestro sitio web -->
 <html lang="es">
 <head>
+    <!-- Se agrego bootstrap para darle estilos -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css" integrity="sha384-BY+fdrpOd3gfeRvTSMT+VUZmA728cfF9Z2G42xpaRkUGu2i3DyzpTURDo5A6CaLK" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- La etiqueta de abajo da el titulo de la pestaña en el navegador -->
     <title>Mi web en php</title>
 </head>
+<!-- Se agrego un color al body de la pagina  -->
 <body class="text-white bg-secondary mb-3">
+    <!-- Se agrego el color a la navegación -->
 <nav class="navbar navbar-dark bg-primary p-3">
+    <!-- aqui se crea el contenedor para centrar el titulo de la navegación -->
     <div class="container text-center">
+        <!-- Se crea el enlace para navegar a la pagina principal -->
         <a href="index.php" class="navbar-brand tex-alin-center">Aplicacion de tareas</a>
     </div>
-
 </nav>
+
